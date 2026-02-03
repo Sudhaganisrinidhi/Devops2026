ac96613e0bd54b3099eb1ecc1102eb9fd4023468
Sudhaganisrinidhi <2303A51342@sru.edu.in>
2026-02-03 10:26:39 +0530

    Add files via upload

---
+++ 3Feb_html_response.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const url = require("url");
+
+http.createServer((req, res) => {
+
+  if (req.url.startsWith("/add")) {
+    const q = url.parse(req.url, true).query;
+    const sum = Number(q.a) + Number(q.b);
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(`Sum is ${sum}`);
+  } else {
+    // Handle other URLs
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Use /add?a=number&b=number");
+  }
+
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
